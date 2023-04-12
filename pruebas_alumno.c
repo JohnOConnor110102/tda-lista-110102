d212118a8c595d51e9a00537f2b33af1483cb597
JohnOConnor110102 <joconnor@fi.uba.ar>
2023-04-11 23:12:24 -0300

    ok (? insertar/en posicion; pos.elemento; destruir

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,59 @@
 #include "pa2m.h"
+#include "src/lista.h"
 
-void prueba_simple()
+void pruebas_de_lista()
 {
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i != j, "i=14 es diferente de j=16");
+	lista_t *lista = lista_crear();
+	pa2m_afirmar(lista != NULL, "la lista creada es distinto de NULL");
+
+	pa2m_afirmar(
+		lista_insertar(NULL, 0) == NULL,
+		"No se puede insertar mediante `lista_insertar` con una lista NULL");
+	lista_insertar(lista, NULL);
+	pa2m_afirmar(
+		lista_elemento_en_posicion(lista, 0) == NULL,
+		"Se puede ingresar un elemento NULL mediante `lista_ingresar`");
+	lista_destruir(lista);
+
+	lista_t *lista1 = lista_crear();
+	int uno = 1, dos = 2, tres = 3;
+	lista_insertar(lista1, &uno);
+	lista_insertar(lista1, &dos);
+	lista_insertar(lista1, &tres);
+	pa2m_afirmar(
+		lista_elemento_en_posicion(lista1, 0) == &uno &&
+			lista_elemento_en_posicion(lista1, 1) == &dos &&
+			lista_elemento_en_posicion(lista1, 2) == &tres,
+		"Se ingresan elementos correctamente mediante `lista_insertar`");
+	lista_destruir(lista1);
+
+	lista_t *lista2 = lista_crear();
+	pa2m_afirmar(
+		lista_insertar_en_posicion(NULL, &uno, 0) == NULL,
+		"No se puede insertar mediante `lista_insertar_en_posicion` con una lista NULL");
+
+	lista_insertar_en_posicion(lista2, NULL, 0);
+	pa2m_afirmar(
+		lista_elemento_en_posicion(lista2, 0) == NULL,
+		"Se puede ingresar un elemento NULL mediante `lista_insertar_en_posicion`");
+	lista_insertar_en_posicion(lista2, &uno, 1);
+	lista_insertar_en_posicion(lista2, &dos, 2);
+	lista_insertar_en_posicion(lista2, &tres, 3);
+	pa2m_afirmar(
+		lista_elemento_en_posicion(lista2, 1) == &uno &&
+			lista_elemento_en_posicion(lista2, 2) == &dos &&
+			lista_elemento_en_posicion(lista2, 3) == &tres,
+		"Se ingresan elementos correctamente mediante `lista_insertar_en_posicion`");
+	lista_destruir(lista2);
 }
 
 int main()
 {
 	pa2m_nuevo_grupo(
-		"\n======================== XXX ========================");
-	prueba_simple();
+		"================= PRUEBAS TDA LISTA =================");
+
+	pa2m_nuevo_grupo("------------- PRUEBAS DE LISTA --------------");
+	pruebas_de_lista();
 
 	return pa2m_mostrar_reporte();
 }
--- src/lista.c
+++ src/lista.c
@@ -2,35 +2,103 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+#define ERROR -1
+#define EXITO 0
+#define MINIMA_POSICION_LISTA 0
+
+const int LISTA_SIN_NODOS = 0;
+
 typedef struct nodo {
 	void *elemento;
 	struct nodo *siguiente;
 } nodo_t;
 
 struct lista {
-	nodo_t *nodo_inicio;
-	//algo mas?
+	nodo_t *inicio;
+	nodo_t *fin;
+	size_t cantidad_nodos;
 };
 
 struct lista_iterador {
-	//y acá?
-	int sarasa;
+	nodo_t *nodo_actual;
+	lista_t *lista;
 };
+
+bool insertar_en_lista_sin_nodos(lista_t *lista, void *elemento, nodo_t *nodo)
+{
+	if (lista == NULL)
+		return false;
+
+	lista->inicio = nodo;
+	lista->fin = nodo;
+	lista->cantidad_nodos++;
+	return true;
+}
+
+nodo_t *iterar_hasta_posicion(lista_t *lista, size_t posicion)
+{
+	size_t posicion_actual = 0;
+	nodo_t *nodo_actual = lista->inicio;
+	nodo_t *nodo_aux;
+	while (posicion_actual < posicion) {
+		nodo_aux = nodo_actual->siguiente;
+		nodo_actual = nodo_aux;
+		posicion_actual++;
+	}
+	return nodo_actual;
+}
 
 lista_t *lista_crear()
 {
-	return NULL;
+	return calloc(1, sizeof(lista_t));
 }
 
 lista_t *lista_insertar(lista_t *lista, void *elemento)
 {
-	return NULL;
+	if (lista == NULL)
+		return NULL;
+
+	nodo_t *nodo = malloc(sizeof(nodo_t));
+	if (nodo == NULL)
+		return NULL;
+	nodo->elemento = elemento;
+	nodo->siguiente = NULL;
+
+	if (lista->cantidad_nodos == LISTA_SIN_NODOS) {
+		insertar_en_lista_sin_nodos(lista, elemento, nodo);
+		return lista;
+	}
+	lista->fin->siguiente = nodo;
+	lista->fin = nodo;
+	lista->cantidad_nodos++;
+	return lista;
 }
 
 lista_t *lista_insertar_en_posicion(lista_t *lista, void *elemento,
 				    size_t posicion)
 {
-	return NULL;
+	if (lista == NULL)
+		return NULL;
+
+	if (posicion >= lista->cantidad_nodos &&
+	    lista_insertar(lista, elemento) != NULL) {
+		return lista;
+	}
+	nodo_t *nodo_a_agregar = malloc(sizeof(nodo_t));
+	if (nodo_a_agregar == NULL)
+		return NULL;
+
+	nodo_a_agregar->elemento = elemento;
+	nodo_a_agregar->siguiente = NULL;
+	if (lista->cantidad_nodos == LISTA_SIN_NODOS) {
+		insertar_en_lista_sin_nodos(lista, elemento, nodo_a_agregar);
+		return lista;
+	}
+	nodo_t *nodo_posicion = iterar_hasta_posicion(lista, posicion);
+	nodo_a_agregar->siguiente = nodo_posicion->siguiente;
+	nodo_posicion->siguiente = nodo_a_agregar;
+
+	return lista;
 }
 
 void *lista_quitar(lista_t *lista)
@@ -45,7 +113,8 @@ void *lista_quitar_de_posicion(lista_t *lista, size_t posicion)
 
 void *lista_elemento_en_posicion(lista_t *lista, size_t posicion)
 {
-	return NULL;
+	nodo_t *nodo = iterar_hasta_posicion(lista, posicion);
+	return nodo->elemento;
 }
 
 void *lista_buscar_elemento(lista_t *lista, int (*comparador)(void *, void *),
@@ -76,6 +145,16 @@ size_t lista_tamanio(lista_t *lista)
 
 void lista_destruir(lista_t *lista)
 {
+	int contador = 0;
+	nodo_t *nodo_actual = lista->inicio;
+	nodo_t *nodo_aux;
+	while (contador < lista->cantidad_nodos) {
+		nodo_aux = nodo_actual->siguiente;
+		free(nodo_actual);
+		nodo_actual = nodo_aux;
+		contador++;
+	}
+	free(lista);
 }
 
 void lista_destruir_todo(lista_t *lista, void (*funcion)(void *))
@@ -84,21 +163,37 @@ void lista_destruir_todo(lista_t *lista, void (*funcion)(void *))
 
 lista_iterador_t *lista_iterador_crear(lista_t *lista)
 {
+	/* 
+	* se crea con malloc
+	* pos actual cero
+	* lista es lista 
+	*/
 	return NULL;
 }
 
 bool lista_iterador_tiene_siguiente(lista_iterador_t *iterador)
 {
+	/* 
+	* si posicion actual es < a cantidad de elementos se puede seguir 
+	* sino no.
+	*/
 	return false;
 }
 
 bool lista_iterador_avanzar(lista_iterador_t *iterador)
 {
+	/* 
+	* posicion actual ++
+	? nodo igual nodo siguiente
+	*/
 	return false;
 }
 
 void *lista_iterador_elemento_actual(lista_iterador_t *iterador)
 {
+	/* 
+	* it lista elementos sub it posicion actual
+	*/
 	return NULL;
 }
 
